44ca3cfb720fef867285dc55743447735a63cbd7
shiyan_xiaobin <shiyan_xiaobin@163.com>
2014-06-22 23:21:56 +0800

    commit

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
-/**
-* 这是入口文件
+/*
+*初始化文件
 */
-echo "This is a start";
+define('STRIVE','jiao');
+define('BASE_PATH', str_replace('\\','/', dirname(__FILE__)));
+if(!@include(dirname(__FILE__).'/global.php')) exit ('global.php isn\'t exit!!');
+if(!@include(BASE_CORE_PATH.'/admin.php')) exit('admin.php isn\' exists!!');
+
 ?>
